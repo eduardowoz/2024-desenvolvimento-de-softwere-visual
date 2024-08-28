dd586abde3a34abe3dd59a10e459df1af70303ab
eduardo <edu.cwozniak@gmail.com>
2024-08-28 11:28:17 -0300

    aula dia 28, e exercicio

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,24 @@
+// Testar as APIs
+// - Rest Client - Extensão do VS Code
+// - Postman
+// - Insomnia
+//MINIMAL APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//EndPoints - Funcionalidades
+//Configurar a URL e o metodo/verbo
+app.MapGet("/", () => "Hello World! em C#");
+app.MapGet("/segundafuncionalidade", () => "segunda funcionalidade da API");
+app.MapGet("/retornarendereco", () => 
+{
+    dynamic endereco = new {
+      rua = "Parigot de Souza",
+      numero = 5300,
+      ceo = "81290-350"  
+    };
+    return endereco;
+});
+
 
 app.Run();
